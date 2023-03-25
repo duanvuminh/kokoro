458be991db347ed1da4499ec20afd212b2c9cc42
DuanVM <duan@guide.inc>
2023-03-25 09:20:09 +0900

    refactor: optimize home page

--- mdx-components.jsx
+++ mdx-components.jsx
@@ -1,5 +1,15 @@
-import Image from 'next/image'
-
-export function useMDXComponents(components) {
-  return { Image: Image,...components };
-}
+// This file allows you to provide custom React components
+// to be used in MDX files. You can import and use any
+// React component you want, including components from
+// other libraries.
+function H1({ children }) {
+    // ...
+  }
+  
+  function H2({ children }) {
+    // ...
+  }
+  
+  export function useMDXComponents(components) {
+    return { h1: H1, h2: H2, ...components };
+  }
--- mdx.d.ts
+++ mdx.d.ts
@@ -1,4 +1,6 @@
 declare module '*.mdx' {
     let MDXComponent: (props: any) => JSX.Element
     export default MDXComponent;
+    export const metadata: any;
+    export const jsonLd: any;
 }
