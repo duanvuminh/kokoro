ed858f76818cfd4bddafff491ae328595da31a1d
DuanVM <duanvuminh@gmail.com>
2023-04-02 14:51:39 +0900

    feat: post's overview

--- lib/part-client/over-view-part-client.tsx
+++ lib/part-client/over-view-part-client.tsx
@@ -4,6 +4,7 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 import { ExpandMoreIcon, SvgPath } from 'lib/type';
 
 
@@ -17,8 +18,14 @@ export function OverViewPartClient({name, children}:{name: string, children: any
           <Typography>Tóm tắt</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Image src={path} alt={id} width='200' height='200'/>
-          {children}
+          <Grid container spacing={2}>
+            <Grid item>
+            <Image src={path} alt={id} width='200' height='200'/>
+            </Grid>
+            <Grid item>
+              {children}
+            </Grid>
+          </Grid>
         </AccordionDetails>
       </Accordion>
     )
