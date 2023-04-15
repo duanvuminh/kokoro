d3ffe9981b376bdcc8889eab9f3f2a379750284c
DuanVM <duanvuminh@gmail.com>
2023-04-16 01:52:50 +0900

    feat(結): kanji 結

--- mdx/mdx-post-content/index.ts
+++ mdx/mdx-post-content/index.ts
@@ -1,8 +1,10 @@
 export * as 日 from "./日.mdx"
+export * as 氏 from "./氏.mdx"
 export * as 結 from "./結.mdx"
 export * as 郎 from "./郎.mdx"
 const paths: string[] = [
 "日",
+"氏",
 "結",
 "郎",
 ]
--- mdx/mdx-post-summary/index.ts
+++ mdx/mdx-post-summary/index.ts
@@ -1,3 +1,4 @@
 export * as 日 from "./日.mdx"
+export * as 氏 from "./氏.mdx"
 export * as 結 from "./結.mdx"
 export * as 郎 from "./郎.mdx"
