de3f0e970c3639f8448c13303072a0353915026c
DuanVM <duanvuminh@gmail.com>
2023-04-16 02:00:28 +0900

    feat(結): kanji 結

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,12 +1,14 @@
 import photo from "./photo.webp"
 import photo_2 from "./photo_2.jpg"
+import photo_2_1 from "./photo_2_1.jpeg"
 import photo_4 from "./photo_4.webp"
 import photo_8 from "./photo_8.jpg"
 import shoukei from "./shoukei.png"
-import ue from "./ue.jpeg"
+import ue from "./ue.png"
 export { 
 photo,
 photo_2,
+photo_2_1,
 photo_4,
 photo_8,
 shoukei,
