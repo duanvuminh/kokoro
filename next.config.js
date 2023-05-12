91dffe70d5ec9157b85cc2856c7307ebb4e1dcf9
DuanVM <duanvuminh@gmail.com>
2023-05-12 10:24:31 +0900

    npm outdated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    mdxRs: true,
-  },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   images: {
     domains: ["kanjivg.tagaini.net"],
   },
