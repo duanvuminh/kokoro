22ea0cee35d705c311b232ffb33a630332f67112
DuanVM <duan@guide.inc>
2023-07-08 08:04:03 +0900

    feat: bf button

--- component/part-client/day-in-month-part-client.tsx
+++ component/part-client/day-in-month-part-client.tsx
@@ -1,3 +1,4 @@
+import { ButtonTextPart } from "component/part";
 import { zeroPad } from "lib/util";
 import { useRouter } from "next/navigation";
 import { Fragment } from "react";
@@ -27,11 +28,25 @@ export function DayInMonthPartClient({
       postId.substring(0, postId.length - 6) + event.target.value;
     router.push(`/post/${postType}/${targetpostId}/`);
   };
+  const targetpostIdNext =
+    postId.substring(0, postId.length - 6) +
+    "day_" +
+    zeroPad((target?.index ?? 1) + 1, 2);
+  const targetpostIdPre =
+    postId.substring(0, postId.length - 6) +
+    "day_" +
+    zeroPad((target?.index ?? 2) - 1, 2);
   return (
     <Fragment>
       <select id="years" defaultValue={target?.id} onChange={onChange}>
         {option}
       </select>
+      <ButtonTextPart href={`/post/${postType}/${targetpostIdNext}/`}>
+        {`day ${target?.index ?? 1 + 1}`}
+      </ButtonTextPart>
+      <ButtonTextPart href={`/post/${postType}/${targetpostIdPre}/`}>
+        {`day ${target?.index ?? 2 - 1}`}
+      </ButtonTextPart>
       {children}
     </Fragment>
   );
