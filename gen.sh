8baaa9dee65117004be5ae778b4d7dff9f4c3653
DUANVM <duanvuminh@gmail.com>
2023-07-09 01:54:30 +0900

    gen.sh を更新

--- gen.sh
+++ gen.sh
@@ -36,21 +36,6 @@ echo 'export {paths}' >>  index.ts
 
 cd ../../
 
-cd mdx/mdx-mean
-rm index.ts
-for f in *.mdx; 
-do echo export '*' as $(basename ${f%.*}) from '"./'$f'"' >> index.ts; 
-done;
-echo   'const paths: string[] = [' >> index.ts;                                            
-for f in *.mdx;
-do
-        echo '"'$(basename ${f%.*})'"'',' >> index.ts
-done;
-echo  ']' >> index.ts
-echo 'export {paths}' >>  index.ts
-
-cd ../../
-
 cd mdx/mdx-kanji-list
 rm index.ts
 for f in *.mdx; 
