8852c882838272b24539015ea9a8f7f5b773149f
DuanVM <duan@guide.inc>
2023-07-09 15:17:24 +0900

    update header

--- component/part-client/header-part-client.tsx
+++ component/part-client/header-part-client.tsx
@@ -1,7 +1,6 @@
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { ButtonTextPart, MenuIconEmptyPart } from "component/part";
 import { useSearch } from "component/part-client";
-import { useRouter } from "next/navigation";
 import { Fragment, useEffect } from "react";
 
 export function TongleMenuPartClient() {
@@ -15,16 +14,14 @@ export function TongleMenuPartClient() {
 }
 1;
 export function MenuIconPartClient(): JSX.Element {
-  const router = useRouter();
   const { isSearchPage } = useSearch();
   return isSearchPage ? (
     <MenuIconEmptyPart />
   ) : (
-    <ButtonTextPart href="#" ariaLabel="tìm kiếm" className="md:invisible">
+    <ButtonTextPart href="/" ariaLabel="tìm kiếm" className="md:invisible">
       <XMarkIcon
         className="block h-6 w-6"
         aria-hidden="true"
-        onClick={() => router.back()}
       />
     </ButtonTextPart>
   );
