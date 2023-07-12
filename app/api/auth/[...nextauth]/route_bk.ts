3209051f5b9c8eb89a414ad0a2cb8ff577327a6e
DuanVM <duan@guide.inc>
2023-07-13 01:32:35 +0900

    npm outdated

--- app/api/auth/[...nextauth]/route_bk.ts
+++ app/api/auth/[...nextauth]/route_bk.ts
@@ -1,9 +1,11 @@
-import { FirestoreAdapter } from "@auth/firebase-adapter"
+import { FirestoreAdapter } from "@next-auth/firebase-adapter"
 import { firestore } from "lib/repository"
 import NextAuth from "next-auth"
 
+
 const handler = NextAuth({
   adapter: FirestoreAdapter(firestore),
+  providers:[]
 })
 
 export { handler as GET, handler as POST }
