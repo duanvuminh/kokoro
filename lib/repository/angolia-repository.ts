13a8cb7b0da2e2b9833034fbf559ad91336ffd7c
DuanVM <duan@guide.inc>
2023-07-28 00:14:51 +0900

    update angolia index mean

--- lib/repository/angolia-repository.ts
+++ lib/repository/angolia-repository.ts
@@ -1,4 +1,4 @@
 const { Angolia } = process.env;
 const algoliasearch = require("algoliasearch");
 export const client = algoliasearch("EI7K5E2KYY", Angolia);
-export const indexAngolia = client.initIndex('kokoro');
+export const indexAngolia = client.initIndex('mean');
