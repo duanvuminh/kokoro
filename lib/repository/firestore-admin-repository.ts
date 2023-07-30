aaf78f6eb28ba33f30660771412dd8874b031a45
DuanVM <duan@guide.inc>
2023-07-30 22:26:01 +0900

    update firebase admin key to env

--- lib/repository/firestore-admin-repository.ts
+++ lib/repository/firestore-admin-repository.ts
@@ -1,12 +1,16 @@
 import * as admin from "firebase-admin";
 import { cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
+export const { FIREBASE_ADMIN_PRIVATE_KEY } = process.env;
 
 var serviceAccount = require("firebase-admin.json");
 
 if (admin.apps.length == 0)
   admin.initializeApp({
-    credential: cert(serviceAccount),
+    credential: cert({
+      ...serviceAccount,
+      privateKey: FIREBASE_ADMIN_PRIVATE_KEY,
+    }),
     databaseURL:
       "https://kyomo-1590f-default-rtdb.asia-southeast1.firebasedatabase.app",
   });
