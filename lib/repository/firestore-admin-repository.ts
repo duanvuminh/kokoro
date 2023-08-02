560236d457adc3d6da94575417e37c6040b84732
DuanVM <duan@guide.inc>
2023-08-02 09:04:26 +0900

    remove firebase admin from dev development

--- lib/repository/firestore-admin-repository.ts
+++ lib/repository/firestore-admin-repository.ts
@@ -2,7 +2,8 @@ import * as admin from "firebase-admin";
 import { cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-var serviceAccount = require("firebase-admin.json");
+const {FIREBASE_ADMIN_PRIVATE_KEY} = process.env;
+var serviceAccount = JSON.parse(FIREBASE_ADMIN_PRIVATE_KEY as string);
 
 if (admin.apps.length == 0)
   admin.initializeApp({
