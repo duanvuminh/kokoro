95d27e725a7520c252dca4cedddc5cf833b4c27e
DuanVM <duan@guide.inc>
2023-09-24 16:11:24 +0900

    update complete remove

--- lib/repository/implement/init-repository.ts
+++ lib/repository/implement/init-repository.ts
@@ -3,13 +3,13 @@ import { IInitRepository, indexAngolia } from "lib/repository";
 @injectable()
 export class InitRepository implements IInitRepository {
   init({ postType, id }: { postType: string; id: string }): void {
-    const saveObject = { objectID: id, level: "n5" };
-    if (postType == "kanji" || postType == "mean")
-      indexAngolia[postType as keyof typeof indexAngolia].partialUpdateObjects(
-        [saveObject],
-        {
-          createIfNotExists: true,
-        }
-      );
+    // const saveObject = { objectID: id, level: "n5" };
+    // if (postType == "kanji" || postType == "mean")
+    //   indexAngolia[postType as keyof typeof indexAngolia].partialUpdateObjects(
+    //     [saveObject],
+    //     {
+    //       createIfNotExists: true,
+    //     }
+    //   );
   }
 }
